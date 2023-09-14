e728a18c9d34ccf32240854be7b23e1219df4ffe
Vattikondateja <89183833+Vattikondateja@users.noreply.github.com>
2023-09-15 02:52:41 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,74 @@
+export const menuArray = [
+  {
+    name: "🟥Pizza",
+    ingredients: ["pepperoni", "mushrooms", "mozzarella"],
+    id: 0,
+    price: 14,
+    emoji: "🍕",
+  },
+  {
+    name: "🟩Pizza",
+    ingredients: ["pepperoni", "mushrooms", "mozzarella"],
+    id: 1,
+    price: 14,
+    emoji: "🍕",
+  },
+  {
+    name: "🍳Pizza",
+    ingredients: ["pepperoni", "mushrooms", "mozzarella"],
+    id: 2,
+    price: 14,
+    emoji: "🍕",
+  },
+
+  {
+    name: "🟥Hamburger",
+    ingredients: ["beef", "cheese", "lettuce"],
+    price: 12,
+    emoji: "🍔",
+    id: 3,
+  },
+  {
+    name: "🟩Hamburger",
+    ingredients: ["beef", "cheese", "lettuce"],
+    price: 12,
+    emoji: "🍔",
+    id: 4,
+  },
+  {
+    name: "🍳Hamburger",
+    ingredients: ["beef", "cheese", "lettuce"],
+    price: 12,
+    emoji: "🍔",
+    id: 5,
+  },
+  
+  {
+    name: "🟥Chicken Wings",
+    ingredients: ["chicken", "spices", "sauce"],
+    id: 6,
+    price: 11,
+    emoji: "🍗",
+  },
+  {
+    name: "🟧Garlic Bread",
+    ingredients: ["bread", "garlic", "butter"],
+    id: 7,
+    price: 5,
+    emoji: "🥖",
+  },
+  {
+    name: "🟣Caprese Salad",
+    ingredients: ["tomatoes", "mozzarella", "basil"],
+    id: 8,
+    price: 7,
+    emoji: "🥗",
+  },
+  {
+    name: "🟤Onion Rings",
+    ingredients: ["onions", "batter", "deep-fried"],
+    id: 9,
+    price: 6,
+    emoji: "🧅",
+  }
+];
